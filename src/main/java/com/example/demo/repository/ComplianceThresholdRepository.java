6533ad4d4ad819fb47d53aea186f3fd779c1517d
bala-amypo <bala@amypo.in>
2025-12-21 16:30:42 +0000

    User commit

--- src/main/java/com/example/demo/repository/ComplianceThresholdRepository.java
+++ src/main/java/com/example/demo/repository/ComplianceThresholdRepository.java
@@ -4,6 +4,8 @@ import com.example.demo.entity.ComplianceLog;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
-public interface ComplianceThresholdRepository extends JpaRepository<ComplianceLog, Long> {
+public interface ComplianceThresholdRepository extends JpaRepository<ComplianceThreshold, Long> {
     List<ComplianceLog> findBySensorReading_Id(Long sensorReadingId);
+    List<ComplianceThreshold> findBySensorType(String sensorType);
+
 }
