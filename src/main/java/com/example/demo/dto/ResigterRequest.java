e96814608ac43d66db662901f8a7cbb2e87391ce
bala-amypo <bala@amypo.in>
2025-12-27 04:25:54 +0000

    User commit

--- src/main/java/com/example/demo/dto/ResigterRequest.java
+++ src/main/java/com/example/demo/dto/ResigterRequest.java
@@ -1,10 +1,10 @@
 package com.example.demo.dto;
 
-public class AuthRequest {
+public class RegisterRequest {
     private String email;
     private String password;
     
-    public AuthRequest() {}
+    public RegisterRequest() {}
     
     public String getEmail() { return email; }
     public void setEmail(String email) { this.email = email; }
